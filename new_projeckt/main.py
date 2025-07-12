c7ead5e01f05641e6946a33f86b8bba5bd6d2061
rnmrul <stack404.dev@gmail.com>
2025-07-12 13:05:07 +0200

    Implement JSON file saving

--- new_projeckt/main.py
+++ new_projeckt/main.py
@@ -1,24 +1,21 @@
 import asyncio
+import json
 import time
-
+import os
 
 import aiohttp
-from fake_useragent import UserAgent
-
 from dotenv import load_dotenv
-import os
+from fake_useragent import UserAgent
 
 
 load_dotenv()
-
 token = os.getenv("GITHUB_TOKEN")
-
-
-repos_lst = []
 ua = UserAgent()
 
 
 async def check_rate_limit(session):
+    """Check current GitHub API rate limit"""
+
     url = "https://api.github.com/rate_limit"
     headers = {
         "Authorization": f"token {token}",
@@ -33,6 +30,8 @@ async def check_rate_limit(session):
 
 
 async def get_page(session, page, per_page=30):
+    """Fetch one page of repositories from GitHub"""
+
     url = "https://api.github.com/search/repositories"
     params = {
             "q": "language:python stars:>500",
@@ -41,13 +40,15 @@ async def get_page(session, page, per_page=30):
             "per_page": per_page,
             "page": page
         }
-    headers = {'user-agent': ua.random,
-                   "Authorization": f"token {token}",
-                   'Accept': 'application/vnd.github+json'
-                   }
+    headers = {
+        'user-agent': ua.random,
+        "Authorization": f"token {token}",
+        'Accept': 'application/vnd.github+json'
+    }
 
     async with session.get(url=url, headers=headers, params=params) as response:
         if response.status == 403:
+            # If rate limit exceeded, wait until reset
             remaining, reset = await check_rate_limit(session)
             await asyncio.sleep(reset + 1)
 
@@ -58,13 +59,14 @@ async def get_page(session, page, per_page=30):
 
 
 async def get_all_pages(page=1, per_page=30):
+    """Fetch all pages of repositories"""
     all_repos = []
-
 
     async with aiohttp.ClientSession() as session:
         while True:
             remaining, reset = await check_rate_limit(session)
             if remaining <= 1:
+                print("[RATE LIMIT] Sleeping...")
                 await asyncio.sleep(reset + 1)
 
             items = await get_page(session, page)
@@ -75,10 +77,9 @@ async def get_all_pages(page=1, per_page=30):
 
             all_repos.extend(items)
             page += 1
-
             await asyncio.sleep(1)
 
-        return all_repos
+    return all_repos
 
 
 async def main():
@@ -91,8 +92,8 @@ async def main():
 
 if __name__ == "__main__":
     all_repos = asyncio.run(main())
+    #print(all_repos)
 
-
-    for r in all_repos:
-        for s in r:
-            print(f"{s['full_name']} - ⭐ {s['stargazers_count']}")
+    result = [s for j in all_repos for s in j]
+    with open("res.json", "w", encoding="utf-8") as file:
+        json.dump(result, file, indent=4, ensure_ascii=False)
